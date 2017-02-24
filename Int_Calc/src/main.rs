a37237dbe1723d208cfd92982d652717822a2073
Phillip Rhinehart <rhinehartphillip@gmail.com>
2017-02-24 12:07:32 -0500

    added comments and made slight changes to code

--- Int_Calc/src/main.rs
+++ Int_Calc/src/main.rs
@@ -1,33 +1,40 @@
 use std::collections::HashMap;
 
-// Calculate the average
+// Calculates the average
 fn average(x: &[u32]) -> f64 {
     let mut sum = 0.0;
     let mut ord = 0.0;
     for element in x.iter() {
-        sum += f64::from(*element); // must dereference 'element' and convert to f64
+        sum += f64::from(*element);
         ord += 1.0;
     }
     sum/ord
 }
-
+//calculates the median
 fn median(x: &mut[u32]) -> f64 {
    x.sort();
+   //if x's length is odd, the midpoint is the quotient
+   //of the penultimate index divded by 2.
    if x.len() % 2 == 1 {
       f64::from(x[(x.len() - 1) / 2])
+   //if even, we take the average of the two mid points
    } else {
-      f64::from((x[x.len() / 2] + x[(x.len() / 2) - 1]) / 2)
+      average(&vec![x[x.len()/2], x[(x.len()/2)-1]])
    }
 }
-
+//calculates the mode
 fn mode(x: &[u32]) -> (u32, u32)  {
    let mut ind = 0;
    let mut max = 0;
+   //Create a map whose keys are elements of x
+   //and whose values are their frequency
    let mut map = HashMap::new();
    for element in x.iter() {
       let count = map.entry(*element).or_insert(0);
       *count += 1;
    }
+   //Find the most frequent key in our map; return it
+   //and its corresponding value as a duple
    for (key, value) in map.iter() {
       if *value > max {
          max = *value;
@@ -39,10 +46,16 @@ fn mode(x: &[u32]) -> (u32, u32)  {
 
 fn main() {
 
-    let mut v = vec![6, 17, 3, 4, 8, 9, 11, 2, 2, 2, 15, 840];
+    let mut v = vec![0];
     let (num, freq) = mode(&v);
 
     println!("The average is {:?}.", average(&v));
     println!("The median is {:?}.", median(&mut v));
-    println!("The mode is {:?}, which occurs {:?} times.", num, freq);
+
+    if freq > 1 {
+      println!("The mode is {:?}, which occurs {:?} times.", num, freq);
+   } else {
+      println!("There is no mode");
+   }
+
 }
